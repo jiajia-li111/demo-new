f648d1b7bf0bfe7b27ada2c07165a3a5b602d3bb
Martin <dyvertin@outlook.com>
2025-11-09 13:34:01 +0800

    Update __init__.py

--- backend/api/routes/__init__.py
+++ backend/api/routes/__init__.py
@@ -1,2 +1,14 @@
+from .auth_routes import auth_bp
+from .health_routes import health_bp
+from .predict_routes import predict_bp
+from .user_routes import user_bp
+
+
+def register_blueprints(app):
+    app.register_blueprint(health_bp)
+    app.register_blueprint(predict_bp)
+    app.register_blueprint(user_bp)
+    app.register_blueprint(auth_bp)
+
 
 
